792c96ae01b46c2a91a19dd8e25534ee8b80907d
YAro <carlend94@ukr.net>
2020-04-10 09:33:05 +0300

    added form grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,79 +19,11 @@ export interface newPost {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  // @ContentChild('info', {static: false}) infoRef: ElementRef;
-  // posts: Post[] = [
-  //   {
-  //     title: 'Want learn Angular Components',
-  //     text: 'I am still learning components',
-  //     id: 1
-  //   },
-  //   {
-  //     title: 'LALA1111111111111111111111111111111222222222222222222',
-  //     text: 'NANA2222222222222222222222222222222222222',
-  //     id: 2
-  //   }
-  // ];
+
 
   ngOnInit(): void {
-    // setTimeout( () => {
-    //   console.log('HELLO')
-    //   this.posts[0] = {
-    //     title: 'CHAAAAAAAAAAAAAANGED',
-    //     text: 'LOOOOOOOOOOOOOOOX'
-    //   }
-    // }, 5000)
   }
 
 
-  // receiveData(myData: Post) {
-  //   this.posts.unshift(myData)
-  //   console.log();
-  //
-  // }
-  //
-  // rem(id: number) {
-  //   console.log(id);
-  //
-  //   this.posts = this.posts.filter(p => p.id !== id)
-  // }
-
-  // ****pipes****
-
-  // num: number = Math.E
-  // str: string = 'Hello, Yarik';
-  // search: string = '';
-  // searchFilter = 'title';
-  //
-  // newPost: newPost[] = [
-  //   {title: 'Beer', name: 'Yarik Pro Proger'},
-  //   {title: 'Coronavirus', name: '2020Hello'},
-  //   {title: 'Third', name: 'CUSTOOOOOOOOOOOOOOm'}
-  // ]
-  //
-  // addNewPost() {
-  //   this.newPost.push({
-  //     title: 'Angular8', name: 'pureChangeDetector'
-  //   });
-  // }
-
-
-
-
-
-
-  // ************************************** ASYNC PIPES *******************************************
-
-  promise: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Promise resolved')
-    }, 5000)
-  })
-
-  observData: Observable<Date> = new Observable<Date>(obs => {
-    setInterval( () => {
-      obs.next(new Date())
-    }, 1000)
-  })
 
 }
