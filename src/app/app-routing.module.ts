a29dd7e27e6d2b2e651bdd641eff74187c75c3dc
YAro <carlend94@ukr.net>
2020-05-03 17:00:25 +0300

    Added LAZY LOADING PreloadingStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from "./app.component";
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
+
 import {HomePageComponent} from "./home-page/home-page.component";
-import {AboutPageComponent} from "./about-page/about-page.component";
+
 
 const routes: Routes = [
-  {path: '', component: AppComponent, pathMatch: 'full'}
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  // {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'} OLD Syntax, Lazy Loading another chunk when click /about page
+  {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule)}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: '', component: HomePageComponent, pathMatch: 'full'},
-    // {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'} OLD Syntax, Lazy Loading another chunk when click /about page
-    {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule)}
-  ])],
+  imports: [RouterModule.forRoot(routes, {
+   preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
+
+// preload Strategy upload first page, than when first page full uploaded, upload next route pages 
 export class AppRoutingModule { }
